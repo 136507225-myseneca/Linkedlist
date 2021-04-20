4f07ab533acd9937a17a572fcb821bd7498bdbf6
tobesegun <officialrubee@gmail.com>
2021-04-21 00:11:03 +0100

    third-commit

--- linklist.js
+++ linklist.js
@@ -22,6 +22,27 @@ class Linkedlsit {
     }
   }
 
+  insertAfter(value, afterValue) {
+    const existingnode = this.find(afterValue)
+    if (existingnode) {
+      const Newnode = { value: value, next: existingnode.next }
+      existingnode.next = Newnode
+    }
+  }
+
+  find(value) {
+    if (!this.head) {
+      return null
+    }
+    let curNode = this.head
+    while (curNode) {
+      if (curNode.value === value) {
+        return curNode
+      }
+      curNode = curNode.next
+    }
+  }
+
   toArray() {
     const content = []
     let curNode = this.head
@@ -31,6 +52,28 @@ class Linkedlsit {
     }
     return content
   }
+
+  delete(value) {
+    if (!this.head) {
+      return
+    }
+    while (this.head && this.head.value === value) {
+      this.head = this.head.next
+    }
+    let curNode = this.head
+
+    while (curNode.next) {
+      if (curNode.next.value === value) {
+        curNode.next = curNode.next.next
+      } else {
+        curNode = curNode.next
+      }
+    }
+
+    if (this.tail.value === value) {
+      this.tail = curNode
+    }
+  }
 }
 const List1 = new Linkedlsit()
 
@@ -38,5 +81,11 @@ List1.append(1)
 List1.append('a')
 List1.append(0.23)
 List1.append('New Car')
+List1.prepend('Car')
 List1.prepend('New Value')
+console.log(List1.toArray())
+List1.delete('a')
+List1.delete('New Car')
+List1.delete('New Value')
+List1.insertAfter(0.21, 0.23)
 console.log(List1.toArray())
